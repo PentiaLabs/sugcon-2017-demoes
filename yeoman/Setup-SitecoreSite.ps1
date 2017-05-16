87fbf5d21f39e94b090da1e72a356d6dd22f2225
Emil Okkels Klein <eok@pentia.dk>
2017-05-16 09:38:56 +0200

    added proper iis site setup, add playbook

--- yeoman/Setup-SitecoreSite.ps1
+++ yeoman/Setup-SitecoreSite.ps1
@@ -56,16 +56,28 @@ Set-Location ..\
 $website = "Sugcon.Website"
 $hostName = "local.Sugcon-demo.nl"
 
-New-WebAppPool -Name $website
-New-Website -Name $website -PhysicalPath C:\websites\sugcon.local\Website -HostHeader $hostName
+Remove-WebAppPool -Name $website
+New-WebAppPool -Name $website -Force
+
+if(-not (Test-Path -Path "C:\websites\sugcon.local\Website"))
+{
+    mkdir C:\websites\sugcon.local\Website
+}
+
+if((Get-Website -Name Sugcon.Website | measure | Select-Object -ExpandProperty Count) -gt 0)
+{
+    Remove-Website -Name $website
+}
+
+New-Website -Name $website -PhysicalPath C:\websites\sugcon.local\Website -HostHeader $hostName -Force
 
 $file = "$env:windir\System32\drivers\etc\hosts"
 if(-not (Get-Content -Path $file -Raw).Contains($hostName))
 {
-	"\r\n127.0.0.1 $hostName" | Add-Content -PassThru $file
+	"`r`n127.0.0.1 $hostName" | Add-Content -PassThru $file
 }
 
 #set configuration files in project
-copy-item -Recurse .\project_files\* .\project\ -Force
+copy-item -Recurse $PSScriptRoot\project_files\* $PSScriptRoot\project\ -Force
 
 $VerbosePreference = $oldPreference
